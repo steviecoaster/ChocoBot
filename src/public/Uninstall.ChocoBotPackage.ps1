3943ccb53ae4d33f4a3e3edf4ade605929014f94
Stephen Valdinger <stephen@chocolatey.io>
2020-12-30 22:12:55 -0500

    Add Uninstall command. Fixup Install/Uninstall output.

--- src/public/Uninstall.ChocoBotPackage.ps1
+++ src/public/Uninstall.ChocoBotPackage.ps1
@@ -1,31 +1,31 @@
-function Install-ChocoBotPackage {
+function Uninstall-ChocoBotPackage {
     <#
     .SYNOPSIS
-    Installs a list of Chocolatey Packages on the target machine(s).
+    Uninstalls a list of Chocolatey Packages on the target machine(s).
     
     .DESCRIPTION
-    This function uses PSRemoting to install the provided list of Chocolatey packages on the target machine(s) specified in the chat message.
+    This function uses PSRemoting to Uninstall the provided list of Chocolatey packages on the target machine(s) specified in the chat message.
     
     .PARAMETER Package
-    The Chocolatey Package(s) to install.
+    The Chocolatey Package(s) to Uninstall.
     
     .PARAMETER Computername
-    The target computer(s) on which to perform the installation of the Chocolatey Package(s).
+    The target computer(s) on which to perform the Uninstallation of the Chocolatey Package(s).
     
     .PARAMETER Source
     If specified, will attempt to locate Chocolatey Packages from this location explicitly.
     
     .PARAMETER Force
-    Will force the installation of the package to occur, even if it is already installed
+    Will force the Uninstallation of the package to occur, even if it is already Uninstalled
     
     .EXAMPLE
-    Install-ChocoBotPackage -Package vlc -Computername Finance01
+    Uninstall-ChocoBotPackage -Package vlc -Computername Finance01
 
     .EXAMPLE
-    Install-ChocoBotPackage -Package vlc,googlechrome,vscode -Computername ((Get-ADComputer -SearchBase "OU=Finance,OU=Chicago","DC=fabrikam",DC=com".Name)
+    Uninstall-ChocoBotPackage -Package vlc,googlechrome,vscode -Computername ((Get-ADComputer -SearchBase "OU=Finance,OU=Chicago","DC=fabrikam",DC=com".Name)
     #>
-    #[PoshBot.BotCommand(CommandName = 'install')]
-    [CmdletBinding(HelpUri="https://github.com/steviecoaster/ChocoBot/blob/main/Help/Install-ChocoBotPackage.md")]
+    #[PoshBot.BotCommand(CommandName = 'Uninstall')]
+    [CmdletBinding(HelpUri="https://github.com/steviecoaster/ChocoBot/blob/main/Help/Uninstall-ChocoBotPackage.md")]
     Param(
         [Parameter(Mandatory)]
         [String[]]
@@ -49,7 +49,7 @@ function Install-ChocoBotPackage {
 
         $chocoArgs = [System.Collections.Generic.List[string]]::new()
         
-        @('install', $Package, '-y', '--no-progress') | ForEach-Object { $chocoArgs.Add($_) }
+        @('uninstall', $Package, '-y') | ForEach-Object { $chocoArgs.Add($_) }
 
         if ($Source) {
             $chocoArgs.Add("--source='$Source'")
@@ -60,7 +60,7 @@ function Install-ChocoBotPackage {
         }
 
         Invoke-ChocoProcess -ChocoArgs $chocoArgs
-
+        
         if($LASTEXITCODE -eq 0){
             [pscustomObject]@{
                 Result = "Success"
@@ -76,6 +76,5 @@ function Install-ChocoBotPackage {
                 Targets = $Computername
             }
         }
-        
     }
 }
